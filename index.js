9d6571b09e43f891a6c5367276f68adac7901b93
fitzgebt <fitzgebt@gmail.com>
2021-04-25 10:37:19 -0700

    commit tree d48df48d2b3527d7e08a8d8b7112d1142a42c05b

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
 // Add your functions here
+let test = [1,2,3,4]
+let test2 = [1,2,-3,4]
+
+function map(sourceArray, callback) {
+    const newArray = []
+    for (let num of sourceArray){
+        newArray.push(callback(num))
+    }
+    return newArray
+};
+
+function reduce(sourceArray, callback, startingPoint) {
+    let total
+    if (startingPoint) {
+        total = startingPoint
+        for (let i=0; i < sourceArray.length; i++) {
+            total = callback(sourceArray[i], total)
+        }
+            // for (let num of sourceArray) {
+                //     total = callback(num, total)
+                // }
+        return total
+    } else {
+        total = sourceArray[0]
+        for (let i=1; i < sourceArray.length; i++) {
+        total = callback(sourceArray[i], total)
+        }
+    
+        // for (let num of sourceArray) {
+        //     total = callback(num, total)
+        // }
+    return total
+    }
+}; 
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+    let total = startingPoint
+    for (let num of sourceArray) {
+        total = total + num
+    }
+    return total
+};
+
+function reduceToAllTrue(sourceArray) {
+    let status = true
+    for (let num of sourceArray) {
+        if (num === false) {
+            status = false;
+        }
+    }
+    return status
+};
+
+function reduceToAnyTrue(sourceArray) {
+    let status = false
+    for (let num of sourceArray) {
+        // debugger
+        if (num === true) {
+            status = true;
+        }
+    }
+    return status
+};
+
